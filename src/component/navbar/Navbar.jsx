4d05df48f46300faddf910e1a39e7187cae24a0d
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-07 07:54:53 +0330

    Add navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
+
+import './Nvbar.css'
+
 import logo from '../../assets/Assets/logo.png'
 import cart_icon from '../../assets/Assets/cart_icon.png'
 const Navbar = () => {
   return (
-    <div>
+    <div className='  navbar  flex  justify-around p-4  '>
       
 <div >
 
 <img src={logo} alt="" />
 <p>SHOPPER</p>
 </div>
+<ul>
 
+<li>Shop</li>
+<li>Men</li>
+<li>Women</li>
+<li>Kids</li>
 
+</ul>
+<div className="login-cart">
+
+<button>Login</button>
+<img src={cart_icon} alt="" />
+</div>
 
     </div>
   )
