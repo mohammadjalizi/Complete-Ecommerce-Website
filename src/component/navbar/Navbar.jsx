b39567970ca3ea45c56a062e5705a0f4251c3369
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-07 08:45:19 +0330

    Add icons count

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Nvbar.css'
 
 import logo from '../../assets/Assets/logo.png'
 import cart_icon from '../../assets/Assets/cart_icon.png'
 const Navbar = () => {
+const[menu,Setmenu]=useState("shop")
+
   return (
     <div className='  navbar  flex  justify-around p-4  '>
       
@@ -15,10 +17,38 @@ const Navbar = () => {
 </div>
 <ul className=' flex  items-center gap-12 text-[#626252] text-[20px] font-medium '>
 
-<li className='flex   flex-col items-center justify-center  gap-1 cursor-pointer' >Shop <hr className=' *: border-none  w-3/4 h-[3px]   rounded-xl	bg-[#FF4141] ' /> </li>
-<li className='flex   flex-col items-center justify-center  gap-1 cursor-pointer' >Men</li>
-<li className='flex   flex-col items-center justify-center  gap-1 cursor-pointer' >Women</li>
-<li className='flex   flex-col items-center justify-center  gap-1 cursor-pointer' >Kids</li>
+<li
+
+onClick={()=>{
+
+Setmenu("shop") 
+
+}}
+className='flex   flex-col items-center justify-center  gap-1 cursor-pointer' >Shop {menu==="shop"?<hr className=' *: border-none  w-3/4 h-[3px]   rounded-xl	bg-[#FF4141] ' />:<></>} </li>
+<li
+
+onClick={()=>{
+
+Setmenu(mens)
+
+}}
+className='flex   flex-col items-center justify-center  gap-1 cursor-pointer' >Men{menu==="mens"?<hr className=' *: border-none  w-3/4 h-[3px]   rounded-xl	bg-[#FF4141] ' />:<></>}</li>
+<li
+
+onClick={()=>{
+
+Setmenu(womens)
+
+}}
+className='flex   flex-col items-center justify-center  gap-1 cursor-pointer' >Women</li>
+<li 
+
+onClick={()=>{
+
+Setmenu(kid)
+
+}}
+className='flex   flex-col items-center justify-center  gap-1 cursor-pointer' >Kids</li>
 
 </ul>
 <div className="login-cart flex  items-center  gap-11 ">
