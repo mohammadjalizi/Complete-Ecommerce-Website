03a7347ee205a4abbaf002ed36c0bcc458b3a1a3
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-07 07:37:27 +0330

    Starting project

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../../assets/Assets/logo.png'
+import cart_icon from '../../assets/Assets/cart_icon.png'
+const Navbar = () => {
+  return (
+    <div>
+      
+
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
