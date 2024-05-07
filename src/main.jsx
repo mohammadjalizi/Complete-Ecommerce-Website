75593ea7a8332066f499955951456d3ecda545da
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-07 17:05:46 +0330

    install react-router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import ShopCactgorey from './pages/ShopCactgorey.jsx';
+import Product from './pages/Product.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/mens" element={<ShopCactgorey   category="men" />} />
+      <Route path="/womens" element={<ShopCactgorey   category="whomen" />} />
+      <Route path="/kids" element={<ShopCactgorey   category="kid" />} />
+      <Route path=":productId" element={<Product />}></Route>
+    </Route>
+  )
+);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/ShopCactgorey.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShopCactgorey = () => {
+  return (
+    <div>ShopCactgorey</div>
+  )
+}
+
+export default ShopCactgorey
