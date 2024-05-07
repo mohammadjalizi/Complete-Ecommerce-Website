c314e79f1d608fd9fa433280b7bd6c713fd5604d
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-07 16:34:34 +0330

    Add icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './component/navbar/Navbar'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
 <Navbar/>
+
     </>
   )
 }
