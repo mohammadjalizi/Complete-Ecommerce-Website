d57b721fa36f76e069a6bb12de88ae8493223595
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-08 16:51:42 +0330

    add  hand icon

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-
+import handicon from '../assets/Assets/hand_icon.png'
 const Hero = () => {
   return (
     <div className=' hero '>
+  
     <div className="hero-left">
+    <h2>NEEW ARRIVALS ONLY</h2>
+    <div>
 
+        <div className="hand-icon">
+<p>new</p>
+<img src={handicon} alt="" />
+        </div>
+    </div>
 
     </div>
     <div className="hero-right">
 
-        
+
     </div>
     
     </div>
