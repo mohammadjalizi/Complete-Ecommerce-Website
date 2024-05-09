c66a2b8819698a3020e3046de4b26a7aac667e6f
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-09 08:33:59 +0330

    add newsleeter file

---
+++ src/newsleter/Newsletter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Newsletter = () => {
+  return (
+    <div className='newsletter'>
+      <h1> GetvExclusive ofeers on Your Email </h1>
+      <p> SSubscribe to our newletter and stay updated </p>
+      <div>
+        <input type="email" placeholder=' Your Email id ' />
+        <button>Subscribe </button>
+      </div>
+    </div>
+  )
+}
+
+export default Newsletter
