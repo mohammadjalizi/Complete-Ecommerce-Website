1c176907d877aa5b4ce73a086533d2cbe659d0d6
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-09 07:26:44 +0330

    add   Array data

--- src/pupler/Popler.jsx
+++ src/pupler/Popler.jsx
@@ -5,8 +5,8 @@ const Popler = () => {
   return (
     <div className=' popler flex flex-col items-center gap-2  h-[90vh]  '>
       <h1 className=' text=[#171717] text-5xl font-semibold' >POPULER IN WOMEN</h1>
-      <hr />
-      <div className="populer-item">
+      <hr  className=' w-[200px] h-2 rounded-lg bg-[#252525] '/>
+      <div className="populer-item mt-12 flex  gap-8">
         {data_product.map((item,i)=>{
 return <Item key={i} id={item.id} name={item.name}  image={item.image} new_Price={item.new_price}  old_price={item. old_price} />
 
