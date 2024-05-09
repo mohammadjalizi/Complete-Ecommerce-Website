05fd8cb84ab7596041bbee05140056247e8da60f
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-09 07:01:58 +0330

    add  file iterm

--- src/item/Item.jsx
+++ src/item/Item.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Item = () => {
   return (
-    <div>Item</div>
+    <div className=' item '>
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+<div className="item-prices">
+  <div className="item-price-new">
+    {Props.new_price}
+
+  </div>
+<div className="item-price-old">
+{props.item_old}
+</div>
+</div>
+
+    </div>
   )
 }
 
