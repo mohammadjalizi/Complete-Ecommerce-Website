196bd006a8248697a92cdcf0ff7e7cd6c28e644c
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-09 08:12:06 +0330

    completed offer file

---
+++ src/newCollection/NewcCollection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import new_collection from '../assets/Assets/new_collections'
+function NewcCollection() {
+  return (
+    <div className=' newcollections'>
+        <h1>NEW COLLECTIONS</h1>
+        <hr />
+        <div className="collections">
+
+
+        </div>
+    </div>
+  )
+}
+
+export default NewcCollection
