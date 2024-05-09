c9a64f7f3cd2c9f9462e7b68ba4484bdaa456b32
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-09 07:48:52 +0330

    completed offer file

--- src/oFFers/Offers.jsx
+++ src/oFFers/Offers.jsx
@@ -4,13 +4,13 @@ import  './Offers.css'
 const Offers = () => {
   return (
     <div className='offers  w-[65%] h-[60vh] flex  m-auto pt-0  px-36 mb-[150px]   '>
-<div className="offers-left">
-<h1>Exclusive</h1>
-<h1>offers For You</h1>
-<p>ONLY ON BEST SELLERS PRODUCTS</p>
-<button> Check Now </button>
+<div className="offers-left flex-1 flex flex-col justify-center ">
+<h1 className=' text-[#171717] text-[80px] font-semibold '>Exclusive</h1>
+<h1 className=' text-[#171717] text-[80px] font-semibold '>offers For You</h1>
+<p className=' colo[#171717] text[22px] font-semibold '>ONLY ON BEST SELLERS PRODUCTS</p>
+<button className=' w-[282px] h-[70px] bg-[#ff4141] border-none  text-white text[22px]  font-medium mt-[30px] cursor-pointer '> Check Now </button>
 </div>
-<div className="offers-right">
+<div className="offers-right flex flex-1 item-center justify-center  pt-[50px]">
 <img src={exclucive_image} alt="" />
 </div>
     </div>
