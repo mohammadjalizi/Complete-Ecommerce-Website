6ae18124901d38db585674a8e6fdac2e4eb5a412
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-09 08:13:20 +0330

    add collections file

--- src/newCollection/NewcCollection.jsx
+++ src/newCollection/NewcCollection.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import new_collection from '../assets/Assets/new_collections'
+import Item from '../item/Item'
 function NewcCollection() {
   return (
     <div className=' newcollections'>
         <h1>NEW COLLECTIONS</h1>
         <hr />
         <div className="collections">
+{new_collection.map((item,i)=>{
+return <Item  key={i} id={item.id} name={item.name}  image={item.image} new_Price={item.new_price}  old_price={item. old_price}/>
 
+})}
 
         </div>
     </div>
