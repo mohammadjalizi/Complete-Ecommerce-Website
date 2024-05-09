5ed43faa0d96b380e0d47750c0f0c21ee943a989
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-09 07:33:25 +0330

    add  file offers

--- src/oFFers/Offers.jsx
+++ src/oFFers/Offers.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import exclucive_image from '../assets/Assets/exclusive_image.png'
 const Offers = () => {
   return (
-    <div className='offers'>
-
-
+    <div className='offers '>
+<div className="offers-left">
+<h1>Exclusive</h1>
+<h1>offers For You</h1>
+<p>ONLY ON BEST SELLERS PRODUCTS</p>
+<button> Check Now </button>
+</div>
+<div className="offers-right">
+<img src={exclucive_image} alt="" />
+</div>
     </div>
   )
 }
